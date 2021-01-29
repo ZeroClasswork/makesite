85195e0b561958b136298feb7f02f93e651fd7a0
atleastzero <megan.obryan52@gmail.com>
2021-01-28 16:03:56 -0800

    Read in file contents; did some reorganizing

--- makesite.go
+++ makesite.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "io/ioutil"
+    "strings"
+)
+
+type Post struct {
+    Title       string
+    Content     string
+}
 
 func main() {
-	fmt.Println("Hello, world!")
+    postContents, err := ioutil.ReadFile("first-post.txt")
+    if err != nil {
+        panic(err)
+    }
+    newPost := new(Post)
+    contentLines := strings.Split(string(postContents), "\n")
+    if len(contentLines) > 0 {
+        newPost.Title = contentLines[0]
+    }
+    for line := range contentLines {
+        if line != 0 && contentLines[line] != "\n" {
+            newPost.Content += contentLines[line]
+        }
+    }
+    fmt.Println("Title")
+    fmt.Println(newPost.Title)
+    fmt.Println("Content")
+    fmt.Println(newPost.Content)
 }

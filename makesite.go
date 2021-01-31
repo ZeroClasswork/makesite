faa7657dc9d8c4f3c4fef2222a4bc1e9d771e152
atleastzero <megan.obryan52@gmail.com>
2021-01-31 15:01:36 -0800

    Created save file and --file flag

--- makesite.go
+++ makesite.go
@@ -1,11 +1,12 @@
 package main
 
 import (
-    "fmt"
+    // "fmt"
     "io/ioutil"
     "strings"
     "html/template"
     "os"
+    "flag"
 )
 
 type Post struct {
@@ -14,7 +15,50 @@ type Post struct {
 }
 
 func main() {
-    postContents, err := ioutil.ReadFile("first-post.txt")
+    // postContents, err := ioutil.ReadFile("first-post.txt")
+    // if err != nil {
+    //     panic(err)
+    // }
+    // newPost := new(Post)
+    // contentLines := strings.Split(string(postContents), "\n")
+    // if len(contentLines) > 0 {
+    //     newPost.Title = contentLines[0]
+    // }
+    // for line := range contentLines {
+    //     if line != 0 && contentLines[line] != "\n" {
+    //         newPost.Contents += contentLines[line]
+    //     }
+    // }
+    // fmt.Println("Title")
+    // fmt.Println(newPost.Title)
+    // fmt.Println("Content")
+    // fmt.Println(newPost.Contents)
+    // fmt.Println("\n\nRead in contents of first-post.txt\n\n")
+
+    // tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+    // err = tmpl.Execute(os.Stdout, newPost)
+    // if err != nil {
+    //     panic(err)
+    // }
+    // fmt.Println("\n\nPrinted first-post.txt with Go Templates\n\n")
+    // newFile, err := os.Create("first-post.html")
+    // if err != nil {
+    //     panic(err)
+    // }
+    // err = tmpl.Execute(newFile, newPost)
+    // if err != nil {
+    //     panic(err)
+    // }
+    // fmt.Println("\n\nWrote HTML template to first-post.html\n\n")
+    var fileName string
+    flagDescription := "a filename of a txt file to be formatted to a post" 
+    flag.StringVar(&fileName, "file", "first-post.txt", flagDescription)
+    flag.Parse()
+    save(fileName)
+}
+
+func save(fileName string) {
+    postContents, err := ioutil.ReadFile(fileName)
     if err != nil {
         panic(err)
     }
@@ -28,19 +72,9 @@ func main() {
             newPost.Contents += contentLines[line]
         }
     }
-    fmt.Println("Title")
-    fmt.Println(newPost.Title)
-    fmt.Println("Content")
-    fmt.Println(newPost.Contents)
-    fmt.Println("\n\nRead in contents of first-post.txt\n\n")
 
     tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-    err = tmpl.Execute(os.Stdout, newPost)
-    if err != nil {
-        panic(err)
-    }
-    fmt.Println("\n\nPrinted first-post.txt with Go Templates\n\n")
-    newFile, err := os.Create("first-post.html")
+    newFile, err := os.Create(fileName[0: len(fileName)-4] + ".html")
     if err != nil {
         panic(err)
     }
@@ -48,5 +82,4 @@ func main() {
     if err != nil {
         panic(err)
     }
-    fmt.Println("\n\nWrote HTML template to first-post.html\n\n")
 }

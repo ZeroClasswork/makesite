ab707789ef5ac500e5d72cb1b5d3da282c157efa
atleastzero <megan.obryan52@gmail.com>
2021-02-02 13:57:49 -0800

    v1.0.0

--- makesite.go
+++ makesite.go
@@ -1,85 +1,50 @@
 package main
 
 import (
-    // "fmt"
-    "io/ioutil"
-    "strings"
-    "html/template"
-    "os"
-    "flag"
+	// "fmt"
+	"flag"
+	"html/template"
+	"io/ioutil"
+	"os"
+	"strings"
 )
 
 type Post struct {
-    Title       string
-    Contents     string
+	Title    string
+	Contents string
 }
 
 func main() {
-    // postContents, err := ioutil.ReadFile("first-post.txt")
-    // if err != nil {
-    //     panic(err)
-    // }
-    // newPost := new(Post)
-    // contentLines := strings.Split(string(postContents), "\n")
-    // if len(contentLines) > 0 {
-    //     newPost.Title = contentLines[0]
-    // }
-    // for line := range contentLines {
-    //     if line != 0 && contentLines[line] != "\n" {
-    //         newPost.Contents += contentLines[line]
-    //     }
-    // }
-    // fmt.Println("Title")
-    // fmt.Println(newPost.Title)
-    // fmt.Println("Content")
-    // fmt.Println(newPost.Contents)
-    // fmt.Println("\n\nRead in contents of first-post.txt\n\n")
-
-    // tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-    // err = tmpl.Execute(os.Stdout, newPost)
-    // if err != nil {
-    //     panic(err)
-    // }
-    // fmt.Println("\n\nPrinted first-post.txt with Go Templates\n\n")
-    // newFile, err := os.Create("first-post.html")
-    // if err != nil {
-    //     panic(err)
-    // }
-    // err = tmpl.Execute(newFile, newPost)
-    // if err != nil {
-    //     panic(err)
-    // }
-    // fmt.Println("\n\nWrote HTML template to first-post.html\n\n")
-    var fileName string
-    flagDescription := "a filename of a txt file to be formatted to a post" 
-    flag.StringVar(&fileName, "file", "first-post.txt", flagDescription)
-    flag.Parse()
-    save(fileName)
+	var fileName string
+	flagDescription := "a filename of a txt file to be formatted to a post"
+	flag.StringVar(&fileName, "file", "first-post.txt", flagDescription)
+	flag.Parse()
+	save(fileName)
 }
 
 func save(fileName string) {
-    postContents, err := ioutil.ReadFile(fileName)
-    if err != nil {
-        panic(err)
-    }
-    newPost := new(Post)
-    contentLines := strings.Split(string(postContents), "\n")
-    if len(contentLines) > 0 {
-        newPost.Title = contentLines[0]
-    }
-    for line := range contentLines {
-        if line != 0 && contentLines[line] != "\n" {
-            newPost.Contents += contentLines[line]
-        }
-    }
+	postContents, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		panic(err)
+	}
+	newPost := new(Post)
+	contentLines := strings.Split(string(postContents), "\n")
+	if len(contentLines) > 0 {
+		newPost.Title = contentLines[0]
+	}
+	for line := range contentLines {
+		if line != 0 && contentLines[line] != "\n" {
+			newPost.Contents += contentLines[line]
+		}
+	}
 
-    tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
-    newFile, err := os.Create(fileName[0: len(fileName)-4] + ".html")
-    if err != nil {
-        panic(err)
-    }
-    err = tmpl.Execute(newFile, newPost)
-    if err != nil {
-        panic(err)
-    }
+	tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
+	newFile, err := os.Create(fileName[0:len(fileName)-4] + ".html")
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(newFile, newPost)
+	if err != nil {
+		panic(err)
+	}
 }

356f0c849e9321c49e115fdc520b7eca00f4b8a4
atleastzero <megan.obryan52@gmail.com>
2021-02-07 11:29:06 -0800

    v1.1.0

--- makesite.go
+++ makesite.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	// "fmt"
 	"flag"
+	// "fmt"
 	"html/template"
 	"io/ioutil"
+	"log"
 	"os"
 	"strings"
 )
@@ -16,16 +17,40 @@ type Post struct {
 
 func main() {
 	var fileName string
-	flagDescription := "a filename of a txt file to be formatted to a post"
-	flag.StringVar(&fileName, "file", "first-post.txt", flagDescription)
+	var dirName string
+	fileFlagDescription := "a filename of a txt file to be formatted to a post"
+	flag.StringVar(&fileName, "file", "", fileFlagDescription)
+	dirFlagDescription := "a directory with txt files to be formatted to posts"
+	flag.StringVar(&dirName, "dir", "", dirFlagDescription)
 	flag.Parse()
-	save(fileName)
+	if fileName != "" {
+		save(fileName)
+	}
+	if dirName != "" {
+		files, err := ioutil.ReadDir(dirName)
+		if err != nil {
+			log.Fatal(err)
+		}
+		for _, file := range files {
+			if file.Name()[len(file.Name())-4:] == ".txt" {
+				// fileContents, err := ioutil.ReadFile(file.Name())
+				// if err != nil {
+				// 	log.Fatal(err)
+				// }
+				// contentLines := strings.Split(string(fileContents), "\n")
+				// for line := range contentLines {
+				// 	fmt.Println(contentLines[line])
+				// }
+				save(file.Name())
+			}
+		}
+	}
 }
 
 func save(fileName string) {
 	postContents, err := ioutil.ReadFile(fileName)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	newPost := new(Post)
 	contentLines := strings.Split(string(postContents), "\n")
@@ -33,18 +58,18 @@ func save(fileName string) {
 		newPost.Title = contentLines[0]
 	}
 	for line := range contentLines {
-		if line != 0 && contentLines[line] != "" {
-			newPost.Contents += template.HTML("<p>" + contentLines[line] + "</p>")
+		if line != 0 && contentLines[line] != "\n" {
+			newPost.Contents += template.HTML("<p>" + contentLines[line] + "</p>\n")
 		}
 	}
 
 	tmpl := template.Must(template.New("template.tmpl").ParseFiles("template.tmpl"))
 	newFile, err := os.Create(fileName[0:len(fileName)-4] + ".html")
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 	err = tmpl.Execute(newFile, newPost)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
 }
